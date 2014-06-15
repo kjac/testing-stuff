aaad13ba6ffa80561e8a89b63a2d400861c672ff
kjac <kja@illumi.dk>
2014-06-15 19:15:07 +0200

    Add missing App_Code

---
+++ SuperWebApp/App_Code/DoSomeComponentStuff.cs
@@ -0,0 +1,14 @@
+﻿using SuperWebApp.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperWebApp.App_Code {
+	public class DoSomeComponentStuff {
+		public static string SomeComponentStuff() {
+			var component = new SomeUmbracoStuff();
+			return component.SomeMethod();
+		}
+	}
+}
